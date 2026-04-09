2301d9795be9e089fce63387abfb5b07323e8421
Magdalena Batres <magdalena.batres@gmail.com>
2026-04-09 18:49:53 -0400

    Actyakuzacuib dek oriyecto

--- index.js
+++ index.js
@@ -5,6 +5,25 @@
 
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1/20;
+
+
+
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX;
+    const y = event.clientY;
+
+    shapes.forEach((shape, i) => {
+        const speed = (i + 1) * 0.05; // cada shape tiene velocidad distinta
+        const direction = i % 2 === 0 ? -1 : 1;
+
+        shape.style.transform = `translate(${x * speed * direction}px, ${y * speed * direction}px)`;
+    });
+}
+
+
 
 function toggleContrast() {
    document.body.classList.toggle("dark-theme");
